c7bfeec45ec68221c316020c5b721a5d785a3c05
Pachingogo <ja6582@hotmail.com>
2026-06-25 15:07:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,6 +63,7 @@ class Percentile_Analysis:
                 start=self.start_date,
                 end=self.end_date,
                 adjustment="all",
+                feed="sip"
                   
             )
             bars = client.get_stock_bars(request)
